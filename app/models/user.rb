2127050bb986aa238fb448cb477196490e87d29d
Tyler Pottle <tyler.pottle@gmail.com>
2015-03-10 15:26:45 -0700

    initial commit

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-  has_secure_password
-  attr_accessible :email, :password, :password_confirmation
-  validates_uniqueness_of :email
-  validates :password, length: { minimum: 6 }
-end
---
+++ auth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '0f35669b5d115f758958bac23e65465d60b12951118244a07ad442ca505e6e74087da542be3c994b9a5062f64d63976a4cf67ef483a50dce5db823f35831db4a'
