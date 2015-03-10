5e387fa301bdb13f713532ae0703f04084f298b4
Tyler Pottle <tyler.pottle@gmail.com>
2015-03-10 15:07:20 -0700

    initial commit of authentication practice

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '47fd19c795b8265af652c828db87b64da438bbb610a5f485c4bd2b1e8ddf6463fa59e7098213f7eb60784c13149a3af62d35e21d9d4568d7d787f3f160e86057'
